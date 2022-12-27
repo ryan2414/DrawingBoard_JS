248292729d57cf2e98c252ad9da57b3498737642
ryan2414 <jackpar@nate.com>
2022-12-27 22:27:55 +0900

    Drawing Project Two

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(210 - 38, 200 - 30, 15, 100);
+ctx.fillRect(350 - 38, 200 - 30, 15, 100);
+ctx.fillRect(260 - 38, 200 - 30, 60, 200);
+ctx.fillRect(230, 370 + 30, 15, 150);
+ctx.fillRect(260, 370 + 30, 15, 150);
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(260, 80, 8, Math.PI, 2 * Math.PI);
+ctx.arc(220, 80, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
