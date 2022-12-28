423586f7cceab1c5a42cdb43007219cb83950df0
ryan2414 <jackpar@nate.com>
2022-12-28 22:44:09 +0900

    Paint Color

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -22,9 +23,12 @@ function cancelPainting() {
   isPainting = false;
   ctx.beginPath();
 }
-
 function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
+}
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -33,3 +37,4 @@ canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
